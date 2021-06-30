904eb300a352186c2eeace70bb3c813f8a82f44c
prasanthgajula08 <prasanth080898@gmail.com>
2021-06-30 02:16:34 -0400

    Add firebase and validation to login

--- my-app/src/Components/LoginPage.js
+++ my-app/src/Components/LoginPage.js
@@ -1,52 +1,88 @@
-import React, { Component } from "react";
-import GoogleLogin from "react-google-login";
+import React, { useState, useEffect } from 'react';
+import db from './firebase'
+import {Redirect} from 'react-router-dom'
 
-export default class Login extends Component {
-    
-    responseGoogle=(response)=>{
-        console.log(response);
+export default function LoginPage() {
+
+    const [userName, setUserName] = useState("");
+    const [password, setPassword] = useState("");
+
+    // responseGoogle=(response)=>{
+    //     console.log(response);
         
+    // }
+
+    const handleUserNameChange = (event) => {
+        setUserName(event.target.value)
+    }
+
+    const handlePasswordChange = (event) => {
+        setPassword(event.target.value)
     }
     
-    render() {
-        return (
-            <div>
-            <form action="/NewsFeed">
-                <h3>Login</h3>
+    const validateUserInput = (event) => {
+        event.preventDefault()
+        // console.log(userName,password)
+        // db.collection('users').onSnapshot(snapshot => {
+        //     console.log(snapshot.docs.map(doc => doc.data()))
+        // })
+        var docRef = db.collection("users").doc(userName);
 
-                <div className="form-group">
-                    <label>Email address</label>
-                    <input type="email" className="form-control" placeholder="Enter email" />
-                </div>
-
-                <div className="form-group">
-                    <label>Password</label>
-                    <input type="password" className="form-control" placeholder="Enter password" />
-                </div>
-
-                <div className="form-group">
-                    <div className="custom-control custom-checkbox">
-                        <input type="checkbox" className="custom-control-input" id="customCheck1" />
-                        <label className="custom-control-label" htmlFor="customCheck1">Remember me</label>
-                    </div>
-                </div>
-
-               <button type="submit" className="btn btn-primary btn-block">Submit</button>
-                <p className="forgot-password text-right">
-                    Forgot <a href="#">password?</a>
-                </p>
-               
-               <div>
-                  <a href="/NewsFeed"> <GoogleLogin
-                   clientId="791659548746-3fejvutdso1a7cvn681ampphavpseqfg.apps.googleusercontent.com"
-                   buttonText="SignIn with Google"
-                   onSuccess={this.responseGoogle}
-                   onFailure={this.responseGoogle}
-                   cookiePolicy={'single_host_origin'}
-                   /> </a>
-                </div>
-            </form>
-            </div>
-        );
+        docRef.get().then((doc) => {
+            if (doc.exists) {
+                if (doc.data().password === password){
+                    console.log("Password correct")
+                    window.location.replace("/NewsFeed");
+                }
+                else{
+                    console.log("Password incorrect")
+                }
+            } else {
+                // doc.data() will be undefined in this case
+                console.log("User doesn't exist");
+            }
+        }).catch((error) => {
+            console.log("Error getting document:", error);
+        });
     }
+
+    return (
+        <div>
+        <form>
+            <h3>Login</h3>
+
+            <div className="form-group">
+                <label>User Name</label>
+                <input type="text" className="form-control" placeholder="Enter User Name" onChange={handleUserNameChange}/>
+            </div>
+
+            <div className="form-group">
+                <label>Password</label>
+                <input type="password" className="form-control" placeholder="Enter password" onChange={handlePasswordChange}/>
+            </div>
+
+            {/* <div className="form-group">
+                <div className="custom-control custom-checkbox">
+                    <input type="checkbox" className="custom-control-input" id="customCheck1" />
+                    <label className="custom-control-label" htmlFor="customCheck1">Remember me</label>
+                </div>
+            </div> */}
+
+           <button onClick={validateUserInput} type="submit" className="btn btn-primary btn-block">Submit</button>
+            <p className="forgot-password text-right">
+                Forgot <a href="#">password?</a>
+            </p>
+           
+           {/* <div>
+              <GoogleLogin
+               clientId="791659548746-3fejvutdso1a7cvn681ampphavpseqfg.apps.googleusercontent.com"
+               buttonText="SignIn with Google"
+               onSuccess={this.responseGoogle}
+               onFailure={this.responseGoogle}
+               cookiePolicy={'single_host_origin'}
+               />
+            </div> */}
+        </form>
+        </div>
+    );
 }
