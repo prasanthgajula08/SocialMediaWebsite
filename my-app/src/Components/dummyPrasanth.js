8407549207f7fd7bf2867e503776017ce7a87c06
prasanthgajula08 <prasanth080898@gmail.com>
2021-07-02 12:33:52 -0400

    Tune NewsFeed and Add dummyPrasanth.js

--- my-app/src/Components/dummyPrasanth.js
+++ my-app/src/Components/dummyPrasanth.js
@@ -22,49 +22,49 @@ export default function NewsFeed() {
 
     useEffect(() => {
         // db.collection('usersData').doc(username)
-        fire.auth().onAuthStateChanged(function(user) {
+        fire.auth().onAuthStateChanged(async function(user) {
             if (user) {
-                setUserName(fire.auth().currentUser.email)
+                await setUserName(fire.auth().currentUser.email)
                 console.log(username)
+                db.collection('usersData').doc(username).collection("posts").onSnapshot(async snapshot => {
+                    await setUsers(snapshot.docs.map(doc => doc.data()))
+                })
+                  docRef = db.collection("usersData").doc(username);
+        
+                  docRef.get().then(async (doc) => {
+                      if (doc.exists) {
+                          console.log(doc.data().posts)
+                          await setJsxFiles(users.map((user) => {
+                            if (doc.data().posts>0) {
+                                return (
+                                    <div>
+                                        <br></br>
+                                        <div class="card" style={{width: '40rem'}}>
+                                            <img src={user.fileURL} class="card-img-top" />
+                                            <div class="card-body">
+                                                <p class="card-title"><strong>{username}</strong></p>
+                                                <p class="card-text">{user.postDescription}</p>
+                                                <button onClick={() => {user.update({likes: user.likes+1})}} type="button" class="btn btn-primary">Like ({user.likes})</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    )
+                            }
+                            else{
+                                return (<div></div>)
+                            }
+                        }))
+                      } else {
+                          // doc.data() will be undefined in this case
+                          console.log("No such document!");
+                      }
+                  }).catch((error) => {
+                      console.log("Error getting document:", error);
+                  });
             } else {
               console.log("user signedout")
             }
           });
-        db.collection('usersData').doc(fire.auth().currentUser.email).collection("posts").onSnapshot(snapshot => {
-            setUsers(snapshot.docs.map(doc => doc.data()))
-        })
-        docRef = db.collection("usersData").doc(fire.auth().currentUser.email);
-        
-        docRef.get().then((doc) => {
-            if (doc.exists) {
-                console.log(doc.data().posts)
-                setJsxFiles(users.map((user) => {
-                if (doc.data().posts>0) {
-                    return (
-                        <div>
-                            <br></br>
-                            <div class="card" style={{width: '40rem'}}>
-                                <img src={user.fileURL} class="card-img-top" />
-                                <div class="card-body">
-                                    <p class="card-title"><strong>{fire.auth().currentUser.email}</strong></p>
-                                    <p class="card-text">{user.postDescription}</p>
-                                    <button onClick={() => {user.update({likes: user.likes+1})}} type="button" class="btn btn-primary">Like ({user.likes})</button>
-                                </div>
-                            </div>
-                        </div>
-                        )
-                }
-                else{
-                    return (<div></div>)
-                }
-            }))
-            } else {
-                // doc.data() will be undefined in this case
-                console.log("No such document!");
-            }
-        }).catch((error) => {
-            console.log("Error getting document:", error);
-        });
     }, [])
 
     var onChangeHandler = async (e) => {
@@ -77,13 +77,14 @@ export default function NewsFeed() {
     };
 
     var submitHandler = async (e) => {
+        console.log(username, jsxFiles)
         e.preventDefault()
-        db.collection('usersData').doc(fire.auth().currentUser.email).collection('posts').doc("0").set({
+        db.collection('usersData').doc(username).collection('posts').doc("0").set({
             fileURL : postImg,
             postDescription: text,
             likes: 0,
         })
-        await db.collection('usersData').doc(fire.auth().currentUser.email).update({
+        await db.collection('usersData').doc(username).update({
             posts: firebase.firestore.FieldValue.increment(1)
         })
         window.location.replace("/NewsFeed");
