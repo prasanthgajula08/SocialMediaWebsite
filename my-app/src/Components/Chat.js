952e27752e6e559aa5e6b699b69238526e8b6e64
prasanthgajula08 <prasanth080898@gmail.com>
2021-07-13 18:52:18 -0500

    update chat.js

--- my-app/src/Components/Chat.js
+++ my-app/src/Components/Chat.js
@@ -6,12 +6,14 @@ import FriendCard from './FriendCard';
 export default function UserChat() {
     const db = fire.firestore()
 
+    const [user, setUser] = useState(null)
     const [following, setFollowing] = useState([])
     let chatList = []
     const [chutList, setChutList] = useState([])
 
     useEffect(() => {
         fire.auth().onAuthStateChanged((authUser) => {
+            setUser(authUser.displayName)
             if(following.length==0){
                 var docRef = db.collection('usersData').doc(authUser.displayName)
                 docRef.get().then((dc) => {
@@ -25,7 +27,6 @@ export default function UserChat() {
                     dcRef.get().then((doc) => {
                         chatList.push(<FriendCard key={index} proPic={doc.data().profilePicture} friendName={friend}/>)
                         if(following.length==chatList.length){
-                            console.log("Equal")
                             setChutList(chatList)
                         }
                     })
@@ -40,7 +41,7 @@ export default function UserChat() {
             <div class="container">
                 <div style={{justifyContent:"center"}} class="row">
                     <div class="col-4">
-                        <div style={{height:"40px", border:"1px solid rgba(39,41,43,0.1)", justifyContent:"center", alignContent:"center"}} class="row">UserName</div>
+                        <div style={{height:"40px", border:"1px solid rgba(39,41,43,0.1)", justifyContent:"center", alignContent:"center"}} class="row">{user}</div>
                         <div style={{overflowY: "auto", height:"780px", border:"1px solid rgba(39,41,43,0.1)", alignContent:"flex-start"}} class="row">
                             {chutList}
                         </div>
